ecd5041849010307eb8d0241ef3e3b86570e944d
Danilo Paggi <danilopaggi@gmail.com>
2020-08-28 10:44:57 -0300

    Partner model creation

---
+++ src/app/domain/models/Partner.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+const PartnerSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true
+  },
+  tradingName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  ownerName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  document: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true
+  },
+  coverageArea:{
+    type: {
+      type: String,
+      enum: ['MultiPolygon'],
+      required: true
+    },
+    coordinates: {
+      type: [[[[Number]]]],
+      required: true
+    }
+  },
+  address: {
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true
+    }
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('Partner', PartnerSchema)
