66097b4dc1d0e97a46eb17427a77a51494189eb9
Danilo Paggi <danilopaggi@gmail.com>
2020-08-30 16:29:53 -0300

    Database connection management

--- tests/integration/database/services/PartnerService.test.js
+++ tests/integration/database/services/PartnerService.test.js
@@ -1,5 +1,7 @@
 const dbConfig = require('../../../../src/database/config');
-const PartnerService = require('../../../../src/database/services/PartnerService');
+const partnerService = require('../../../../src/database/services/PartnerService');
+
+const dBUriObj = dbConfig.getDBUriObj();
 
 const testPartner = {
   id: '3b241101-e2bb-4255-8caf-4136c566a962',
@@ -19,51 +21,75 @@ const testPartner = {
   }
 }
 
-describe('Test Describe.', () => {
-  it('Test test.', async () => {
-    expect(testPartner.id).toBe(testPartner.id);
+describe('Validation of the flow to add a new partner.', () => {
+  beforeAll(async () => {
+    await dbConfig.createConnection(dBUriObj);
   });
-});
 
-/*describe('Validation of the flow to add a new partner.', () => {
+  afterAll(async () => {
+    await dbConfig.endConnection();
+  });
+
   it('Should be able to add a valid new partner.', async () => {
-    const newPartnerCode = await PartnerService.setNewPartner(testPartner);
-    expect(testPartner.id).toBe(newPartnerCode);
+    const newPartnerCode = await partnerService.setNewPartner(testPartner);
+
+    expect(newPartnerCode.isSuccess).toBeTruthy();
+    expect(testPartner.id).toBe(newPartnerCode.id);
   });
   it('Should not be able to add a new partner with an existing id.', async () => {
-    const newPartnerCode = await PartnerService.setNewPartner({...testPartner, document: '1111111111111/1111'});
-    expect(newPartnerCode).toBeNull();
-    expect(newPartnerCode).toBeUndefined();
+    const newPartnerCode = await partnerService.setNewPartner({...testPartner, document: '1111111111111/1111'});
+
+    expect(newPartnerCode.isSuccess).toBeFalsy();
+    expect(newPartnerCode.message.indexOf("duplicate key")).toBeGreaterThanOrEqual(0);
   });
   it('Should not be able to add a new partner with an existing document.', async () => {
-    const newPartnerCode = await PartnerService.setNewPartner({...testPartner, id: '3b241101-e2bb-4255-8caf-4136c566a963'});
-    expect(newPartnerCode).toBeNull();
-    expect(newPartnerCode).toBeUndefined();
+    const newPartnerCode = await partnerService.setNewPartner({...testPartner, id: '3b241101-e2bb-4255-8caf-4136c566a963'});
+
+    expect(newPartnerCode.isSuccess).toBeFalsy();
+    expect(newPartnerCode.message.indexOf("duplicate key")).toBeGreaterThanOrEqual(0);
   });
 });
 
 describe('Validation of the flow to find a specific partner.', () => {
+  beforeAll(async () => {
+    await dbConfig.createConnection(dBUriObj);
+  });
+
+  afterAll(async () => {
+    await dbConfig.endConnection();
+  });
+
   it('Should be able to find an existing partner.', async () => {
-    const partner = await PartnerService.getPartnerById('3b241101-e2bb-4255-8caf-4136c566a962');
+    const partner = await partnerService.getPartnerById('3b241101-e2bb-4255-8caf-4136c566a962');
+
     expect(partner).toBeTruthy();
     expect(partner.id).toBe(testPartner.id);
     expect(partner.ownerName).toBe(testPartner.ownerName);
     expect(partner.document).toBe(testPartner.document);
   });
   it('Should not be able to return an unknown partner id.', async () => {
-    const partner = await PartnerService.getPartnerById('99999999-9999-9999-9999-999999999999');
-    expect(partner).toBe(testPartner);
+    const partner = await partnerService.getPartnerById('99999999-9999-9999-9999-999999999999');
+
+    expect(partner).toBeFalsy();
   });
 });
 
 describe('Validation of the flow to delete a specific partner.', () => {
+  beforeAll(async () => {
+    await dbConfig.createConnection(dBUriObj);
+  });
+
+  afterAll(async () => {
+    await dbConfig.endConnection();
+  });
+
   it('Should be able to delete an existing partner.', async () => {
-    const beforeDeletePartner = await PartnerService.getPartnerById('3b241101-e2bb-4255-8caf-4136c566a962');
-    await PartnerService.deletePartnerById('3b241101-e2bb-4255-8caf-4136c566a962');
-    const afterDeletePartner = await PartnerService.getPartnerById('3b241101-e2bb-4255-8caf-4136c566a962');
+    const beforeDeletePartner = await partnerService.getPartnerById('3b241101-e2bb-4255-8caf-4136c566a962');
+    await partnerService.deletePartnerById('3b241101-e2bb-4255-8caf-4136c566a962');
+    const afterDeletePartner = await partnerService.getPartnerById('3b241101-e2bb-4255-8caf-4136c566a962');
 
     expect(beforeDeletePartner.id).toBe(testPartner.id);
     expect(beforeDeletePartner).toBeTruthy();
     expect(afterDeletePartner).toBeFalsy();
   });
-});*/
+});
