1ecdcdc8975b9777d44b941b3408fcfd14beba31
Danilo Paggi <danilopaggi@gmail.com>
2020-08-31 22:44:12 -0300

    Closest partner search in database

--- src/database/services/GeoJsonDBServices.js
+++ src/database/services/GeoJsonDBServices.js
@@ -1,1 +1,27 @@
-//TBD
+const Partner = require('../schemas/PartnerSchema');
+const IGeoJsonMethods = require('../../app/domain/contracts/IGeoJsonMethods');
+
+const GeoJsonDBServices = Object.assign({}, IGeoJsonMethods);
+
+GeoJsonDBServices.nearestPartner = async coordinates => (
+  await Partner.aggregate([
+    {
+      $geoNear: {
+        near: { type: "Point", coordinates },
+        key: "address",
+        distanceField: "dist.calculated",
+        query: {
+          coverageArea: {
+            $geoIntersects: {
+              $geometry: { type: "Point", coordinates }
+            }
+          }
+        },
+        spherical: true
+      }
+    },
+    { $limit: 1 }
+  ])
+);
+
+module.exports = GeoJsonDBServices;
